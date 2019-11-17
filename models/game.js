862fe7453a15aea635aac36422737e6cd60e9ead
deeem <karpov.dimon@gmail.com>
2019-11-17 09:59:36 +0200

    add random method to GamesSchema

--- models/game.js
+++ models/game.js
@@ -16,4 +16,12 @@ const GamesSchema = new mongoose.Schema(
   }
 )
 
+GamesSchema.statics.random = async function() {
+  const count = await this.countDocuments();
+  const rand = Math.floor(Math.random() * count);
+  const randomDoc = await this.findOne().skip(rand);
+
+  return randomDoc;
+}
+
 module.exports = mongoose.model('Game', GamesSchema)
