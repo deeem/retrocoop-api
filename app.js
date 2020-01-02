415b4ade56da84df1b2c5a36ce01a35debb9dade
deeem <karpov.dimon@gmail.com>
2020-01-02 06:38:05 +0200

    add cors middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const path = require('path')
 const colors = require('colors')
+const cors = require('cors')
 
 const connectDB = require('./config/db')
 const errorHandler = require('./middlewares/errorHandler')
@@ -18,6 +19,8 @@ connectDB()
 app = express()
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(cors())
 
 app.use('/api/games', games)
 app.use('/api/requests', requests)
